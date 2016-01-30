a34cf9123077921be39085873a7e6a64a2efe11f
ricardobqueiroz <ricardobqueiroz@gmail.com>
2016-01-29 22:50:46 -0300

    Adicionando o resto dos assets.

---
+++ Source/Assets/Scripts/DragTransform.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragTransform : MonoBehaviour {
+    private Color mouseOverColor = Color.blue;
+    private Color originalColor = Color.yellow;
+    private bool dragging = false;
+    private float distance;
+    public Collider moveCollider;
+
+
+    void OnMouseEnter()
+    {
+        //renderer.material.color = mouseOverColor;
+        Debug.Log("Mouse enter");
+    }
+
+    void OnMouseExit()
+    {
+        //renderer.material.color = originalColor;
+        Debug.Log("Mouse exit");
+    }
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.useGravity = true;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+
+            //transform.position = rayPoint;
+            RaycastHit hitInfo;
+            bool hit = moveCollider.Raycast(ray, out hitInfo,1000);
+            if (hit)
+            {
+                Rigidbody rb = GetComponent<Rigidbody>();
+                rb.MovePosition(Vector3.Lerp(hitInfo.point,rb.position,0.5f));
+                //transform.position = hitInfo.point;
+            }
+        }
+    }
+}
---
+++ Source/Assets/Scripts/WordGenerator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WordGenerator : MonoBehaviour
+{
+
+    public List<string> prefix = new List<string>();
+    public List<string> middle = new List<string>();
+    public List<string> suffix = new List<string>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Generate()
+    {
+        string result = prefix[Random.Range(0, prefix.Count)] +
+                     prefix[Random.Range(0, prefix.Count)] +
+                     prefix[Random.Range(0, prefix.Count)];
+        Debug.Log(result);
+    }
+}
