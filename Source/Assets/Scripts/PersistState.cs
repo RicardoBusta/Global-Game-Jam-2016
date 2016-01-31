6da5f57ddf1e660f7ca6d34d76f3ed54d8c39dbc
ricardobqueiroz <ricardobqueiroz@gmail.com>
2016-01-31 00:04:25 -0300

    botando variavel no persist state

--- Source/Assets/Scripts/PersistState.cs
+++ Source/Assets/Scripts/PersistState.cs
@@ -1,40 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PersistState : MonoBehaviour {
+public class PersistState : MonoBehaviour
+{
 
-  private static PersistState state;
+    private static PersistState state;
 
-  [HideInInspector] public int stage;
-  [HideInInspector] public int score;
+    [HideInInspector]
+    public int stage;
+    [HideInInspector]
+    public int score;
+
+    public int startLevel = 1;
 
 
-	void Start () {
-    // singleton
-    if(state==null){
-      state = this;
-      DontDestroyOnLoad(gameObject);
-    }else{
-      Destroy(gameObject);
+    void Start()
+    {
+        // singleton
+        if (state == null)
+        {
+            state = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        // initialize
+        ResetGame();
     }
 
-    // initialize
-    ResetGame();
-	}
-
-  public static PersistState GetPersistState(){
-    if(state==null){
-      state = GameObject.FindWithTag("Persist State").GetComponent<PersistState>();
+    public static PersistState GetPersistState()
+    {
+        if (state == null)
+        {
+            state = GameObject.FindWithTag("Persist State").GetComponent<PersistState>();
+        }
+        return state;
     }
-    return state;
-  }
 
-  void ResetGame(){
-    stage = 1;
-    score = 0;
-  }
-	
-	void Update () {
-	
-	}
+    void ResetGame()
+    {
+        stage = startLevel;
+        score = 0;
+    }
+
+    void Update()
+    {
+
+    }
 }
