07a8f923ff139b3335e7903ebb850b454f7eb546
ricardobqueiroz <ricardobqueiroz@gmail.com>
2016-01-31 15:40:09 -0300

    Toma

---
+++ Source/Assets/Scripts/Breath.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Breath : MonoBehaviour {	
+	// Update is called once per frame
+  public float breathSpeed = 0.1f;
+  float angle = 0;
+  public float intensityX = 0.001f;
+  public float intensityY = 0.001f;
+  Vector3 originalScale;
+
+  void Start()
+  {
+    originalScale = transform.localScale;
+  }
+
+	void Update () {
+    angle += breathSpeed;
+
+    transform.localScale = originalScale + new Vector3(intensityX * Mathf.Cos(angle), intensityY * Mathf.Sin(angle), 0);
+	}
+}
